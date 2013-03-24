830ec7bb09eeefcede0d82f5657a8197cc7069aa
ccymbor <ccymbor@gmail.com>
2013-03-24 12:36:10 -0700

    init

---
+++ BlizzardCodeEval_MessageDecoder/JScript1.js
@@ -0,0 +1,54 @@
+﻿var maxHeaderLength = 7;
+var maxHeaderBinary = 3;
+
+function toBinary(k, key_len) {
+    var bin_pos_amt = [1], bin_key = [];
+    for (var i = 1; i < key_len; i++) { bin_pos_amt[i] = bin_pos_amt[i - 1] * 2; }
+    for (var i = key_len - 1; i >= 0; i--) { if (bin_pos_amt[i] > k) { bin_key.push(0); } else { bin_key.push(1); k -= bin_pos_amt[i]; } }
+    if (k > 0) alert('binary conversion err: number exceeds bit type');
+    return bin_key.join('');
+}
+
+function fromBinary(digits, _len) {
+    digits = String(digits); if (digits.length != _len) alert('integer conversion err: bit type mismatch');
+    var bin_pos_amt = [1], _num = 0;
+    for (var i = 1; i < _len; i++) { bin_pos_amt[i] = bin_pos_amt[i - 1] * 2; }
+    for (var i = 0; i < _len; i++) { if (digits.charAt(_len - 1 - i) == 1) _num += bin_pos_amt[i]; }
+    return _num;
+}
+
+function end_seq(work_len) { var seq = ''; for (var i = 0; i < work_len; i++) { seq += String(1); } return seq; }
+
+function Init_Dec(header, message) 
+{
+    if (header.length > maxHeaderLength) alert('decode err: invalid header specified');
+    var keys = [], cur_key = 0, key_len = 1, seg_num_keys = 1; 		//generate the scheme from header
+    for (var i = 0; i <= header.length - 1; i++) {
+        keys[i] = toBinary(cur_key, key_len);
+
+        cur_key++;
+        if (cur_key == seg_num_keys) { cur_key = 0; seg_num_keys *= 2; seg_num_keys += 1; key_len++; }
+
+    }
+
+    var chars = [], work_pos = 0, work_len = 0, work_str = '';
+
+    for (var i = 0; i <= message.length - 1; i++) {
+
+        work_pos++; work_str += String(message.charAt(i));
+        if (work_len == 0 && work_pos == maxHeaderBinary) { work_len = fromBinary(work_str, maxHeaderBinary); work_pos = 0; work_str = ''; }
+
+
+        if (work_len != 0) {
+            if (work_pos == work_len) {
+                if (work_str == end_seq(work_len)) {
+                    work_len = 0; work_pos = 0; work_str = '';
+                } else {
+                    chars.push(header.charAt(keys.indexOf(work_str))); work_pos = 0; work_str = '';
+                }
+            }
+        }
+    }
+
+    document.write(chars.join('')); alert(chars.join(''));
+}
---
+++ BlizzardCodeEval_MessageDecoder/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BlizzardCodeEval_MessageDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> keyList = new List<String>();
+            InitKeyList(keyList);
+
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line) continue;
+
+                    //split line into header and message portions
+                    int messageStart = line.IndexOf('0');
+                    if (messageStart == 0) messageStart = line.IndexOf('1');
+
+                    string header = line.Substring(0, messageStart);
+                    string message = line.Substring(messageStart);
+
+                    List<string> keys = new List<string>();
+                    int keyLength = 0;
+
+                    do
+                    {
+                        //get length indicator
+                        keyLength = Convert.ToInt32(message.Substring(0, 3), 2);
+
+                        //message is done, move on to the next
+                        if (keyLength == 0) continue;
+
+                        //get segment end string
+                        string segmentEndIndicator = String.Empty.PadLeft(keyLength, '1');
+                        int segmentEndIndex = 0;
+
+                        //i = 3 so that we skip the length indicator portion of the segment
+                        for (int i = 3; i < message.Length; i += keyLength)
+                        {
+                            string key = message.Substring(i, keyLength);
+                            if (key == segmentEndIndicator) 
+                            {
+                                segmentEndIndex = i;
+                                break;
+                            }
+                            else
+                            {
+                                keys.Add(key);
+                            }
+                        }
+
+                        //remove the segment just processed from the message
+                        message = message.Substring(segmentEndIndex + segmentEndIndicator.Length);
+
+                    } while (keyLength > 0);
+
+
+                    string decodedMessage = string.Empty;
+                    foreach (string key in keys)
+                    {
+                        decodedMessage += header[keyList.IndexOf(key)];
+                    }
+
+                    Console.WriteLine(decodedMessage);
+                }
+
+            Console.ReadKey();
+        }
+
+        private static void InitKeyList(List<string> keyList)
+        {
+            //setup map for the number of keys and the length of each key up to the maximum of 7
+            Dictionary<int, int> keyCreationMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                int length = i * 2 + 1;
+                keyCreationMap.Add(length, i + 1);
+            }
+
+            foreach (KeyValuePair<int, int> keyValuePair in keyCreationMap)
+            {
+                for (int i = 0; i < keyValuePair.Key; i++)
+                {
+                    string binResult = Convert.ToString(i, 2);
+                    //pad to appropriate length
+                    keyList.Add(binResult.PadLeft(keyValuePair.Value, '0'));
+                }
+            }
+        }
+    }
+}
